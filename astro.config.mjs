7d423dc4ee012cf5e70df905a6e396bb294671b4
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2025-05-22 17:04:29 -0500

    Integrate Netlify adapter and update site URL; enhance global styles with new font and lazy loading for hero image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	vite: {
-		plugins: [tailwindcss()],
+  site: 'https://nidoperulina.netlify.app/',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  adapter: netlify(),
 });
