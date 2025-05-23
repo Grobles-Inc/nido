8b7d2cadfafadef5a0a7fc2a3402fc2c06141a40
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2025-05-22 20:10:37 -0500

    Enhance project configuration and components: add React integration, update dependencies, and redesign footer with contact information and social links. Include new sections in the homepage layout for About, Services, and Home.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nidoperulina.netlify.app/',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
 
   vite: {
       plugins: [tailwindcss()],
