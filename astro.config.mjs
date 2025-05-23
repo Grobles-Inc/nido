5d8ffae3168655d69c6edbb28ae4aa78fc5d1a55
kralion <joan300501@gmail.com>
2025-05-23 11:22:57 -0500

    removed react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nidoperulina.netlify.app/',
-  integrations: [mdx(), sitemap(), react()],
-
+  site: 'https://nidoperulina.netlify.app',
+  integrations: [mdx(), sitemap()],
   vite: {
       plugins: [tailwindcss()],
 	},
-
   adapter: netlify(),
 });
